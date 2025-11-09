e17b00ee2dd3a233010f867ad8080720fec9b95c
«StasyStar» <Anstar1991@gmail.com>
2025-11-09 20:34:22 +0300

    Initial Commit

---
+++ SoulBattle/SoulBattleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SoulBattleApp.swift
+//  SoulBattle
+//
+//  Created by Anastasia on 09.11.2025.
+//
+
+import SwiftUI
+
+@main
+struct SoulBattleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
